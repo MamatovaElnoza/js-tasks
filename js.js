4044b85caf43a01d4ee30cf6b5f05491d4b5f027
ElnozaMamatova <mamatovaelnoza@gmail.com>
2024-10-17 17:29:02 +0500

    fizzbuzz with function

--- js.js
+++ js.js
@@ -1,4 +1,22 @@
 
+// // function fizzBuzz(number) {
+// //     if (number % 3 === 0 && number % 5 === 0) {
+// //         console.log('fizzbuzz');
+// //     }
+// //     else if (number % 3 === 0) {
+// //         console.log('fizz');
+// //     }
+// //     else if (number % 5 === 0) {
+// //         console.log('buzz')
+// //     }
+// //     else {console.log(number);
+// //     }
+// // }
+// // fizzBuzz(2)
+
+
+
+
 // // function cacl(amal, number1, number2) {
 // //     if (amal == '-') {
 // //         console.log(number1 - number2);
