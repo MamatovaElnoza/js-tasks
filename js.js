96b294d55133bb9e2050a0f35a3541083ea3babf
ElnozaMamatova <mamatovaelnoza@gmail.com>
2024-10-17 17:30:05 +0500

    fizzbuzz with for loop

--- js.js
+++ js.js
@@ -1,4 +1,24 @@
 
+// for (let number = 19; number <= 100; number++) {
+//     if (number % 3 === 0 && number % 5 === 0) {
+//         console.log('fizzbuzz');
+//     }
+//     else if (number % 3 === 0) {
+//         console.log('fizz');
+//     }
+//     else if (number % 5 === 0) {
+//         console.log('buzz')
+//     }
+//     else {
+//         console.log(number);
+//     }
+// }
+
+
+
+
+
+
 // // function fizzBuzz(number) {
 // //     if (number % 3 === 0 && number % 5 === 0) {
 // //         console.log('fizzbuzz');
