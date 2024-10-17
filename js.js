57f9c3ae90e1712e8fd0e97c52443cc0ab89f8f8
ElnozaMamatova <mamatovaelnoza@gmail.com>
2024-10-17 17:27:59 +0500

    calculator

---
+++ js.js
@@ -0,0 +1,17 @@
+
+// // function cacl(amal, number1, number2) {
+// //     if (amal == '-') {
+// //         console.log(number1 - number2);
+// //     }
+// //     else if (amal == '+') {
+// //         console.log(number1 + number2);
+// //     }
+// //     else if (amal == '*') {
+// //         console.log(number1 * number2);
+// //     }
+// //     else if (amal == '/') {
+// //         console.log(number1 / number2);
+// //     }
+// // }
+// // cacl('/', 7, 2)
+// // cacl('+', 9, 2)
